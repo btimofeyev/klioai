d1e8dc4cca155afb8c79397f32ae48dbe4fe41b3
ben timofeyev <btimofeyev@gmail.com>
2024-12-12 10:04:28 -0500

    updated url

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,7 +10,7 @@ const ChatState = {
 const API = {
     // Environment-based URL configuration
     baseUrl: window.location.hostname === 'localhost' 
-        ? 'http://localhost:3002/api'
+        ? 'http://localhost:3000/api'
         : 'https://klioai.com/api',
 
     async request(endpoint, options = {}) {
